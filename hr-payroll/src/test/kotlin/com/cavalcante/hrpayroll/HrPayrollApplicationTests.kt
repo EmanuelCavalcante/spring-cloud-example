36f04f39cb1fd022c2c24f02f46ffb36ba12b97b
Emanuel <emanuellima3105@gmail.com>
2021-10-10 19:09:40 -0300

    Created project hr-payroll

---
+++ hr-payroll/src/test/kotlin/com/cavalcante/hrpayroll/HrPayrollApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cavalcante.hrpayroll
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
