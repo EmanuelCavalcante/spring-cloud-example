aab0ee4b0bd6968358e08a9c1c267aa4ff4a0879
Emanuel <emanuellima3105@gmail.com>
2021-10-11 15:01:57 -0300

    Implement project zull api gateway

---
+++ hr-api-gateway-zuul/src/main/kotlin/com/cavalcante/hrapigatewayzuul/HrApiGatewayZuulApplication.kt
@@ -0,0 +1,15 @@
+package com.cavalcante.hrapigatewayzuul
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+
+@EnableEurekaClient
+@EnableZuulProxy
+@SpringBootApplication
+class HrApiGatewayZuulApplication
+
+fun main(args: Array<String>) {
+    runApplication<HrApiGatewayZuulApplication>(*args)
+}
