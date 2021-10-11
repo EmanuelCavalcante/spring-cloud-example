7c8a5d19b722c69abdabcbc288e9e078fd9bd367
Emanuel <emanuellima3105@gmail.com>
2021-10-11 12:19:22 -0300

    Configuration hystrix

--- hr-payroll/src/main/kotlin/com/cavalcante/hrpayroll/resources/PaymentResource.kt
+++ hr-payroll/src/main/kotlin/com/cavalcante/hrpayroll/resources/PaymentResource.kt
@@ -2,6 +2,7 @@ package com.cavalcante.hrpayroll.resources
 
 import com.cavalcante.hrpayroll.entities.Payment
 import com.cavalcante.hrpayroll.services.PaymentService
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/payments")
 class PaymentResource(private val paymentService: PaymentService) {
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workId}/days/{days}")
     fun getPayment(
         @PathVariable("workId") workId: Long,
@@ -21,4 +23,10 @@ class PaymentResource(private val paymentService: PaymentService) {
         return ResponseEntity.ok(response)
     }
 
+    fun getPaymentAlternative(
+        workId: Long,
+        days: Int
+    ): ResponseEntity<Payment> {
+        return ResponseEntity.ok(Payment("Jose", 10.0, 10))
+    }
 }
